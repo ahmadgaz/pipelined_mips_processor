c80308cd4c2f6d3552064bc99ccdc4b741ed4ce1
ahmadgaz <gazali.contact@gmail.com>
2025-12-05 22:22:22 -0800

    :P

--- datapath/alu.v
+++ datapath/alu.v
@@ -1,8 +1,8 @@
 module alu (
-    input wire [ 2:0] op,
-    input wire [31:0] a,
-    input wire [31:0] b,
-    input wire [4:0] shamt,
+    input  wire [ 2:0] op,
+    input  wire [31:0] a,
+    input  wire [31:0] b,
+    input  wire [ 4:0] shamt,
     output wire        zero,
     output reg  [31:0] y
 );
@@ -10,13 +10,13 @@ module alu (
 
   always @(op, a, b, shamt) begin
     case (op)
-      3'b000: y = a & b;
-      3'b001: y = a | b;
-      3'b010: y = a + b;
-      3'b110: y = a - b;
-      3'b111: y = (a < b) ? 1 : 0;
-      3'b100: y = a << shamt;
-      3'b101: y = a >> shamt;
+      3'b000:  y = a & b;
+      3'b001:  y = a | b;
+      3'b010:  y = a + b;
+      3'b110:  y = a - b;
+      3'b111:  y = (a < b) ? 1 : 0;
+      3'b100:  y = b << shamt;
+      3'b101:  y = b >> shamt;
       default: y = 32'b0;
     endcase
   end
